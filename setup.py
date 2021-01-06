dcbaea59632de31e0c665be50358f983def3f835
Madrone Goldman <mgoldman@gavant.com>
2021-01-06 13:16:16 -0500

    setup update

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='dynamo_auth',
     version='0.0.1',
+    url='https://github.com/mgoldman-gavant/dynamo_auth',
     install_requires=[
         "sqlalchemy-aurora-data-api==0.2.7",
         "aurora-data-api==0.2.7",
